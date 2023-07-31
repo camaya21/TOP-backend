4fae75a01fcdfcedc140f1901a344925975a39d1
camaya21 <cindymamaya21@gmail.com>
2023-07-31 14:00:21 -0400

    Added update route

--- controllers/symptom-controller.js
+++ controllers/symptom-controller.js
@@ -30,4 +30,10 @@ router.delete("/:id", async (req, res) => {
 	res.status(200).json({message: "symptoms delete route: " + req.params.id })
 });
 
+// SYMPTOMS UPDATE ROUTE
+router.put("/:id", async (req, res) => {
+	console.log(req.body)
+	res.status(200).json({message: "symptoms update route: " + req.params.id })
+});
+
 module.exports = router
