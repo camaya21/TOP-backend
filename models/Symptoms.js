7fa301b3379884a367e5c806b14ba816680b938d
camaya21 <cindymamaya21@gmail.com>
2023-07-31 14:53:00 -0400

    full crud backend api

--- models/Symptoms.js
+++ models/Symptoms.js
@@ -7,16 +7,16 @@ const mongoose = require("mongoose");
 // MODELS
 ////////////////////////////////
 const SymptomsSchema = new mongoose.Schema({
-    Date: { type: String, require: true },
-    Age: { type: String, require: true },
-    Sex: { type: String, require: true },
-    Race: { type: String, require: true },
-    Ethnicity: { type: String, require: true },
-    Zipcode: { type: String, require: true },
-    DOB: { type: String, require: true },
-    Address: { type: String, require: true },
-    PhoneNumber: { type: String, require: true },
-    MedicalHistory: { type: String, require: true },
+    date: { type: String, require: true },
+    age: { type: String, require: true },
+    sex: { type: String, require: true },
+    race: { type: String, require: true },
+    ethnicity: { type: String, require: true },
+    zipcode: { type: String, require: true },
+    dob: { type: String, require: true },
+    address: { type: String, require: true },
+    phoneNumber: { type: String, require: true },
+    medicalHistory: { type: String, require: true },
 },{timestamps: true});
 
 const Symptoms = mongoose.model("Symptoms", SymptomsSchema);
