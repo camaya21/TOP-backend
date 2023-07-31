6f2b45e16540c2374fecb89be3ce35d1ad105cf7
camaya21 <cindymamaya21@gmail.com>
2023-07-31 17:57:39 -0400

    Changed dob to dateOfBirth

--- models/Symptoms.js
+++ models/Symptoms.js
@@ -13,7 +13,7 @@ const SymptomsSchema = new mongoose.Schema({
     race: { type: String, require: true },
     ethnicity: { type: String, require: true },
     zipcode: { type: String, require: true },
-    dob: { type: String, require: true },
+    dateOfBirth: { type: String, require: true },
     address: { type: String, require: true },
     phoneNumber: { type: String, require: true },
     medicalHistory: { type: String, require: true },
