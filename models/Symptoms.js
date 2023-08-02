8b59d7ac80c7299216d997eb3feb4fcb3d9b617d
camaya21 <cindymamaya21@gmail.com>
2023-08-01 22:14:09 -0400

    Added symptoms to schema

--- models/Symptoms.js
+++ models/Symptoms.js
@@ -7,6 +7,21 @@ const mongoose = require("mongoose");
 // MODELS
 ////////////////////////////////
 const SymptomsSchema = new mongoose.Schema({
+    cough: Boolean,
+    headache: Boolean,
+    chills: Boolean,
+    diarrhea: Boolean,
+    conjunctivitis: Boolean,
+    fever: Boolean,
+    muscleAche: Boolean,
+    excessiveFatigue: Boolean,
+    abdominalPain: Boolean,
+    other: Boolean,
+    troubleBreathing: Boolean,
+    lossOfSmell: Boolean,
+    nauseaVomiting: Boolean,
+    skinRash: Boolean,
+    noSymptoms: Boolean,
     date: { type: String, require: true },
     age: { type: String, require: true },
     sex: { type: String, require: true },
