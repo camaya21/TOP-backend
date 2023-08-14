db1bde926a685e4e944e207ee9b244bec16494e1
camaya21 <cindymamaya21@gmail.com>
2023-08-14 15:38:01 -0400

    Added Test Delete Route

--- controllers/test-controller.js
+++ controllers/test-controller.js
@@ -14,8 +14,8 @@ require("../config/db.connection");
 // TEST INDEX ROUTE
 router.get('/', async (req,res)=>{ 
     try {
-        const symptomsIndex = await Symptoms.find({})
-        return res.status(200).json(symptomsIndex)
+        const testIndex = await Test.find({})
+        return res.status(200).json(testIndex)
     } catch(error) {
         console.error(error)
         return next(error)
@@ -25,8 +25,8 @@ router.get('/', async (req,res)=>{
 // TEST POST ROUTE
 router.post('/', async(req, res)=>{
     try{
-        const newSymptoms = await Symptoms.create(req.body)
-        res.status(201).json(newSymptoms)
+        const newTest = await Test.create(req.body)
+        res.status(201).json(newTest)
     }catch(err){
         res.status(400).json ({error:err.message})
     }
@@ -34,11 +34,22 @@ router.post('/', async(req, res)=>{
 // TEST SHOW ROUTE
 router.get('/:id', async(req,res)=>{
     try{
-        const findSymptoms = await Symptoms.findById(req.params.id)
-        res.status(201).json(findSymptoms)
+        const findTest = await Test.findById(req.params.id)
+        res.status(201).json(findTest)
     }catch(err){
         res.status(400).json({error: err.message})
     }
 })
+
+// TEST DELETE ROUTE
+router.delete('/:id', async(req,res)=>{
+    try{
+        const deletedTest = await Test.findByIdAndDelete(req.params.id)
+        res.status(201).json(deletedTest)
+    }catch(err){
+        res.status(400).json({error: err.message})
+    }
+})
+
 
 
